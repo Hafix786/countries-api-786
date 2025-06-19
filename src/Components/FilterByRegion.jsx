782f52e165cb9cde284247f2c2c410fa481260d5
Abdur Rehman <ibnkhalid786@gmail.com>
2025-06-20 02:01:16 +0500

    correct the region amreica to americas

--- src/Components/FilterByRegion.jsx
+++ src/Components/FilterByRegion.jsx
@@ -6,7 +6,7 @@ const FilterByRegion = ({setQuery}) => {
       <select onChange={(e) => setQuery(e.target.value.toLowerCase())}>
         <option hidden="">Filter by Region</option>
         <option value="Africa">Africa</option>
-        <option value="America">America</option>
+        <option value="Americas">Americas</option>
         <option value="Asia">Asia</option>
         <option value="Antarctic">Antarctic</option>
         <option value="Europe">Europe</option>
